bce183bef7e79f3fa696abe1783ef95094cba324
Vitaly Zaitsev <vitaly@easycoding.org>
2016-09-04 20:44:50 +0500

    Added arguments parser.

--- ql2pn.py
+++ ql2pn.py
@@ -1,3 +1,19 @@
 #!/usr/bin/env python
 # coding=utf-8
 
+import argparse
+
+
+def mkparser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--logdir', type=str, default='', help='Path to Q2005a log directory.', required=True)
+    parser.add_argument('--resdir', type=str, default='', help='Path to output directory.', required=True)
+    return parser
+
+
+def main():
+    n = mkparser().parse_args()
+
+
+if __name__ == '__main__':
+    main()
